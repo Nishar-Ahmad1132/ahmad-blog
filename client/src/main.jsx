f98b416c2e899b4ff56974ad2fb02434e0a85526
Nishar Ahmad <ahmadnishar1132@gmail.com>
2024-01-27 00:19:19 +0530

    redux persist added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,18 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import {store} from './redux/store.js'
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
+import { persistor, store } from "./redux/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    ,
+  </PersistGate>
+);
