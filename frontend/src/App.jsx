33c078523d42324670050304dac0148070c0b1f7
Nishar Ahmad <ahmadnishar1132@gmail.com>
2024-09-12 19:04:34 +0530

    api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 
-  import { ToastContainer } from "react-toastify";
-  import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
